22cf2905ba7b1da38621f9b32076da3d158deb1b
seydazenler <seydaazenler@gmail.com>
2022-10-02 23:53:51 +0300

    Business katmanına Abstract metotlar eklendi. Tema proje içerisine dahil edildi.

---
+++ BusinessLayer/Concrete/AboutManager.cs
@@ -0,0 +1,47 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class AboutManager : IAboutService
+    {
+        IAboutDal _aboutDal;
+
+        //Constructor metot -- yapıcı metot
+        public AboutManager(IAboutDal aboutDal)
+        {
+            _aboutDal = aboutDal;
+        }
+        public void TAdd(About t)
+        {
+            _aboutDal.Insert(t);
+        }
+
+        public void TDelete(About t)
+        {
+            _aboutDal.Delete(t);
+        }
+
+        public About TGetByID(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<About> TGetList()
+        {
+            //void olmayan metot
+           return _aboutDal.GetList();
+        }
+
+        public void TUpdate(About t)
+        {
+            _aboutDal.Update(t);
+        }
+    }
+}
